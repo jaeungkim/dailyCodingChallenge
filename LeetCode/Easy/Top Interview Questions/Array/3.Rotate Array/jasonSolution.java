b622603e714ac6cc0f755ff75a57177705a3eb7f
Jason Yoon <cjrzl9198@hotmail.com>
2021-08-15 21:53:24 -0700

    Daily LeetCode Challenge for Saturday and Sunday

--- LeetCode/Easy/Top Interview Questions/Array/3.Rotate Array/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Array/3.Rotate Array/jasonSolution.java
@@ -1,2 +1,16 @@
 class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        k = k % nums.length;
+        int[] newAry = Arrays.copyOfRange(nums, nums.length-k, nums.length);
+        for(int i = nums.length-1; i >= 0 ; i--){
+            int newIndex = i-k;
+            if(newIndex < 0){
+                nums[i] = newAry[i];
+            }
+            else {
+                nums[i] = nums[newIndex];
+            }
+        }
+    }
 }
--- LeetCode/Easy/Top Interview Questions/Array/4.Contains Duplicate/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Array/4.Contains Duplicate/jasonSolution.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
 class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Boolean> hash = new HashMap<Integer, Boolean>();
+        for(int i = 0; i < nums.length; i++){
+            if(hash.containsKey(nums[i]))
+                return true;
+            
+            else 
+                hash.put(nums[i], true);
+            
+        }
+        return false;
+    }
 }
