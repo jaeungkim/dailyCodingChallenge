eafbd89e9c7f45d6561b15e392b7afefe03df05a
jaeungkim <jaeung95@student.ubc.ca>
2021-08-16 14:11:02 -0700

    2021-08-16 contain duplicate(array)

--- LeetCode/Easy/Top Interview Questions/Array/4.Contains Duplicate/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/Array/4.Contains Duplicate/jaeSolution.java
@@ -1,3 +1,20 @@
 public class jaeSolution {
-    
+    //Given an integer array nums, 
+    //return true if any value appears at least twice in the array, 
+    //and return false if every element is distinct.
+    //ex) Input: nums = [1,2,3,1]
+    //output: true (duplicate exists)
+
+    public boolean containDuplicate(int[] nums) {
+
+        //need nested loop to go through all array elements with each index
+        for(int i=0; i<nums.length; i++){ //i=0,1,2,3 , nums.length = 4 (4 times)
+            for(int j=1; j<nums.length; j++){ //j=1,2,3, (3 times)
+                if(nums[i] == nums[j]){ // compare nums[0] with nums[1] , nums[2] , nums[3]
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
