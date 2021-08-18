c6ef4611b2c7c51a3df4af4a2ba5424165e0b407
Jason Yoon <cjrzl9198@hotmail.com>
2021-08-17 20:26:47 -0700

    Daily LeetCode challenge for Aug 17

--- LeetCode/Easy/Top Interview Questions/Array/5.Single Number/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Array/5.Single Number/jasonSolution.java
@@ -1,2 +1,10 @@
+// Used optimal solution from Submission
 class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i ++){
+            result ^= nums[i];
+        }
+        return result;
+    }
 }
