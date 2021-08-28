3df719669ab9e92d7aa3113c0f6d3c377b0d61fd
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-08-27 21:48:20 -0700

    daily coding

--- LeetCode/Easy/Top Interview Questions/Array/7.Plus One/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Array/7.Plus One/jasonSolution.java
@@ -1,4 +1,19 @@
-// Used optimal solution from Submission
 class Solution {
-
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length-1; i >= 0 ; i--){
+            if(digits[i] == 9 && i == 0){
+                int[] newAry = new int[digits.length+1];
+                newAry[0] = 1;
+                return newAry;
+            }
+            else if(digits[i] == 9){
+                digits[i] = 0;
+            }
+            else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        return digits;
+    }
 }
