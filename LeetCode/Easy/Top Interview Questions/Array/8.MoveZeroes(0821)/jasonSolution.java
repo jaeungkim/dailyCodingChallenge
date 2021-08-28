cc5792dc0715ccd492be665dad2ab60a55c862b5
Jason Yoon <cjrzl9198@hotmail.com>
2021-08-28 00:59:52 -0700

    sh: q: command not found

--- LeetCode/Easy/Top Interview Questions/Array/8.MoveZeroes(0821)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Array/8.MoveZeroes(0821)/jasonSolution.java
@@ -1,3 +1,18 @@
 class Solution {
-
+    public void moveZeroes(int[] nums) {
+        
+        int index = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]!=0){
+                nums[index] = nums[i];
+                index++;
+            }
+            else 
+                count++;
+        }
+        for(int i = nums.length-count; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
 }
