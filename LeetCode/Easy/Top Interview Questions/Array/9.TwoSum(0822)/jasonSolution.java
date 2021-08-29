2b7ecd777244c52d0ef9bb2a9d47677859e9d32c
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-08-29 15:44:24 -0700

    daily coding Challenge

--- LeetCode/Easy/Top Interview Questions/Array/9.TwoSum(0822)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Array/9.TwoSum(0822)/jasonSolution.java
@@ -1,3 +1,16 @@
 class Solution {
-
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<Integer, Integer>();
+        for( int i = 0; i < nums.length; i++){
+            hash.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(hash.containsKey(target-nums[i])){
+                if(hash.get(target-nums[i]) != i){
+                    return new int[] {i, hash.get(target-nums[i])};
+                }
+            }
+        }
+        return new int[2];
+    }
 }
