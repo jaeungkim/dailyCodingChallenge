ec8b8b4f2879d67133a99d9daa4b8d07ca128249
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-06 01:41:32 -0700

    bs problem

--- LeetCode/Easy/Top Interview Questions/Strings/2.Reverse Integer(0826)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Strings/2.Reverse Integer(0826)/jasonSolution.java
@@ -1,3 +1,26 @@
+// couldn't solve this problem
 class Solution {
+    public int reverse(int x) {
+        String s = "" + x;
+        boolean isNegative = false;
+        if(s.charAt(0) == '-'){
+            isNegative = true;
+            s = s.substring(1, s.length());
+        }
+        char[] ary = s.toCharArray();
 
+        
+        for(int i = 0; i < ary.length/2; i++){
+            char temp = ary[i];
+            ary[i] = ary[ary.length-1-i];
+            ary[ary.length-1-i] = temp;
+        }
+        if(isNegative)
+            s = "-";
+        else 
+            s = "";
+        s = s + new String(ary);
+        x = Integer.parseInt(s);
+        return x;
+    }
 }
