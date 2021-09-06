7292d747d59b4fc0216f70429b9dee7d9d7d493f
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-05 23:26:43 -0700

    Coding challenge

--- LeetCode/Easy/Top Interview Questions/Strings/1.Reverse String(0825)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/Strings/1.Reverse String(0825)/jasonSolution.java
@@ -1,3 +1,13 @@
 class Solution {
-
+    public void reverseString(char[] s) {
+        for(int i = 0; i < s.length/2; i++){ 
+            swap(s, i, s.length-i-1);
+        }
+    }
+    
+    public void swap(char[] s, int i, int j){
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 }
