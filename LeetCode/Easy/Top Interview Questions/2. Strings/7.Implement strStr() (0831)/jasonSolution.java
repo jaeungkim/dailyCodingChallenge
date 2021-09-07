dfbc22ae2b143a0a77b54aa955060dad5830aa87
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-07 16:53:58 -0700

    Daily coding Challenge

--- LeetCode/Easy/Top Interview Questions/2. Strings/7.Implement strStr() (0831)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/7.Implement strStr() (0831)/jasonSolution.java
@@ -1,3 +1,21 @@
+// Brute force solution
 class Solution {
-
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0)
+            return 0;
+        for(int i = 0 ; i < (haystack.length()-needle.length()+1); i++) {
+            
+            for(int j = 0; j < needle.length(); j++){
+                char h = haystack.charAt(i+j);
+                char n = needle.charAt(j);
+                if(h != n)
+                    break;
+                
+                if(j == needle.length()-1)
+                    return i;
+            }
+            
+        }
+        return -1;
+    }
 }
