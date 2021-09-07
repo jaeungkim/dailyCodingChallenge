048fa2ae860c894f6d27f39cb05282c863683800
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-07 15:29:32 -0700

    daily coding Challenge

--- LeetCode/Easy/Top Interview Questions/2. Strings/5.Valid Palindrome(0829)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/5.Valid Palindrome(0829)/jasonSolution.java
@@ -1,3 +1,18 @@
 class Solution {
+    public boolean isPalindrome(String s) {
 
+        s = s.toLowerCase();
+        String onlyLetters = "";
+        for(int i = 0 ; i < s.length(); i++){
+            char c = s.charAt(i);
+            if((c >= 'a' && c <= 'z' )||(c >= '0' && c <= '9'))
+                onlyLetters +=  c+"";
+        }
+        
+        for(int i = 0; i < onlyLetters.length()/2; i++){
+            if(onlyLetters.charAt(i)!= onlyLetters.charAt(onlyLetters.length()-1-i))
+                return false;
+        }
+        return true;
+    }
 }
