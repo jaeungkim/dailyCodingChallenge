1fe2467a6a4fee193c61c6562083348d75ba5870
Jason Yoon <cjrzl9198@hotmail.com>
2021-09-07 13:03:36 -0700

    Daily coding Challenge

--- LeetCode/Easy/Top Interview Questions/2. Strings/3.First Unique Character in a String(0827)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/3.First Unique Character in a String(0827)/jasonSolution.java
@@ -1,3 +1,20 @@
 class Solution {
-
+    public int firstUniqChar(String s) {
+        HashMap hash = new HashMap<Character, Integer>();
+        for(int i = 0; i < s.length(); i++){
+            char curr = s.charAt(i);
+            if(hash.containsKey(curr)){
+                hash.put(curr, -1);
+            }
+            else{
+                hash.put(curr, i);
+            }
+        }
+        for(int i = 0; i < s.length(); i++){
+            int val = (int) hash.get(s.charAt(i));
+            if(val != -1)
+                return val; 
+        }
+        return -1;
+    }
 }
