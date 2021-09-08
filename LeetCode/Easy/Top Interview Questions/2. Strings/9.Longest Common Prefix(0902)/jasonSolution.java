3451f3c349b803d16a84f292483fa631f515cb74
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-08 16:29:44 -0700

    Daily coding Challenge

--- LeetCode/Easy/Top Interview Questions/2. Strings/9.Longest Common Prefix(0902)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/9.Longest Common Prefix(0902)/jasonSolution.java
@@ -1,3 +1,18 @@
 class Solution {
-
+    public String longestCommonPrefix(String[] strs) {
+        
+        String prefix = strs[0];
+        Boolean changed = true;
+        while(prefix.length() != 0 && changed){
+            changed = false;
+            for(int i = 0; i < strs.length; i++){
+                if(strs[i].indexOf(prefix)!=0){
+                    prefix = prefix.substring(0, prefix.length()-1);
+                    changed = true;
+                }
+            }
+        }
+        return prefix;
+    }
+    
 }
