569a859fd3099a6ac80236615ef028a4c11389f7
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-08 16:08:37 -0700

    daily coding Challenge

--- LeetCode/Easy/Top Interview Questions/2. Strings/8.Count and Say(0901)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/8.Count and Say(0901)/jasonSolution.java
@@ -1,3 +1,32 @@
 class Solution {
-
+    public String countAndSay(int n) {
+        String[] fib = new String[n];
+        fib[0] = "1";
+        for(int i = 1; i < n; i++){
+            if(fib[i] == null){
+                fib[i] = say(fib[i-1]);
+            }
+        }
+        return fib[n-1];
+    }
+    
+    public String say(String nums){
+        char prev = nums.charAt(0);
+        int len = 1;
+        StringBuilder sb= new StringBuilder();
+        for(int i = 1; i < nums.length(); i++){
+            char curr = nums.charAt(i);
+            if(prev != curr){
+                sb.append(len+""+prev);
+                len = 1;
+                prev = curr;
+            }
+            else {
+                len++;
+            }        
+        }
+        sb.append(len+""+prev);
+        
+        return sb.toString();
+    }
 }
