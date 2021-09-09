cf5c2806b2703e9dd4acc82599d7bba81031b193
jaeungkim <jaeung95@student.ubc.ca>
2021-09-08 23:05:25 -0700

    210908
    catching up lotta stuff to catch up :(

--- LeetCode/Easy/Top Interview Questions/1. Array/8.MoveZeroes(0821)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/1. Array/8.MoveZeroes(0821)/jaeSolution.java
@@ -1,3 +1,19 @@
 public class jaeSolution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        // {0,1,0,3,12} -> {1,3,12,0,0}
+        //
+        // move zeros to the end
+        int nonzeros = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i]; // store temp value with nonzero iterate
+                nums[i] = nums[nonzeros]; // assign nums[i] value with nums[nonzero]
+                nums[nonzeros] = temp; // assign nums[nonzero] to temp value that was nonzero
+                nonzeros++; // increment zero
 
+            }
+        }
+
+    }
 }
