cef4a9d6223e01442164d03599aeb45bbcabb6ad
jaeungkim <jaeung95@student.ubc.ca>
2021-09-09 13:36:01 -0700

    210909
    catching up

--- LeetCode/Easy/Top Interview Questions/1. Array/8.MoveZeroes(0821)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/1. Array/8.MoveZeroes(0821)/jaeSolution.java
@@ -2,7 +2,6 @@ public class jaeSolution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
         // {0,1,0,3,12} -> {1,3,12,0,0}
-        //
         // move zeros to the end
         int nonzeros = 0;
         for (int i = 0; i < n; i++) {
@@ -11,7 +10,6 @@ public class jaeSolution {
                 nums[i] = nums[nonzeros]; // assign nums[i] value with nums[nonzero]
                 nums[nonzeros] = temp; // assign nums[nonzero] to temp value that was nonzero
                 nonzeros++; // increment zero
-
             }
         }
 
--- LeetCode/Easy/Top Interview Questions/1. Array/9.TwoSum(0822)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/1. Array/9.TwoSum(0822)/jaeSolution.java
@@ -1,3 +1,15 @@
 public class jaeSolution {
-
+    //pretty straight forward answer won't add descriptions
+    
+    int[] temp = new int[2];
+        
+    for (int i = 0 ; i < nums.length; i++){
+        for (int j = i+1; j < nums.length; j++){
+            if(target-nums[i] == nums[j]){
+                temp[0] = i;
+                temp[1] = j;
+            }
+        }
+    }
+    return temp;
 }
