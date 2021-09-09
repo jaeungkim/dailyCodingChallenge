2582187ef75871f1f399b82f1592bccfca7095c9
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-08 18:01:58 -0700

    Daily coding Challenge

--- LeetCode/Easy/Top Interview Questions/3. Linked List/2. Remove Nth Node from End of List (0904)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/3. Linked List/2. Remove Nth Node from End of List (0904)/jasonSolution.java
@@ -1,3 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int len = 0;
+        ListNode copy = head;
+        // Checking the length of the list
+        while(copy != null){
+            len++;
+            copy = copy.next;
+        }
+        if(len == n)
+            return head.next;
+        // Nth node
+        len -= n;
+        
+        copy = head;
+        for(int i = 0; i < len-1; i++)
+            copy = copy.next;
+        
+        copy.next = copy.next.next;
+        return head;
+    }
 }
