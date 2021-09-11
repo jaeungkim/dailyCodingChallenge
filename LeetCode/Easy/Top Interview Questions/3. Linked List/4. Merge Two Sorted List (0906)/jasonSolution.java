1db7b5a6695d7f398fd4e3148c73fc324b204e5f
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-10 23:29:53 -0700

    daily coding challenge

--- LeetCode/Easy/Top Interview Questions/3. Linked List/4. Merge Two Sorted List (0906)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/3. Linked List/4. Merge Two Sorted List (0906)/jasonSolution.java
@@ -1,3 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        if(l1 == null && l2 == null)
+            return l1;
+        
+        ListNode head = null;
+        ListNode curr = null;
+        if(checkVal(l1) > checkVal(l2)){
+            head = l2;
+            curr = l2;
+            l2 = l2.next;
+        }
+        else {
+            head = l1;
+            curr = l1;
+            l1 = l1.next;
+        }
+        while(l1 != null || l2 != null){
+            if(checkVal(l1) < checkVal(l2)){
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+    
+    public int checkVal(ListNode node){
+        if(node == null)
+            return 101;
+        return node.val;
+    }
 }
