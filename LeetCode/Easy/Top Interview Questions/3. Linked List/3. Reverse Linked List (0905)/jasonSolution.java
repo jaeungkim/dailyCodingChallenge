16efe27971c1556f719e7fb3881588e69119900b
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-10 18:42:48 -0700

    daily coding challege

--- LeetCode/Easy/Top Interview Questions/3. Linked List/3. Reverse Linked List (0905)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/3. Linked List/3. Reverse Linked List (0905)/jasonSolution.java
@@ -1,3 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-
+    public ListNode reverseList(ListNode head) {
+        if(head == null)
+            return head;
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            if(next == null)
+                return head;
+            head = next;
+        }
+        return head;
+    }
 }
