d2cadec77383bc8b26e4d6a6453d56f65637d764
jaeungkim <jaeung95@student.ubc.ca>
2021-09-12 15:15:51 -0700

    2109012 upto string 2.2 catching up

--- LeetCode/Easy/Top Interview Questions/1. Array/10.Valid Sudoku(0823)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/1. Array/10.Valid Sudoku(0823)/jaeSolution.java
@@ -1,24 +1,3 @@
 public class jaeSolution {
-    // Write a function that reverses a string. The input string is given as an
-    // array of characters s.
-    // Input: s = ["h","e","l","l","o"]
-    // Output: ["o","l","l","e","h"]
 
-    //reverse string
-    if (s == null){
-        return;
-    }
-    
-    int i = 0;
-    int j = s.length -1; //4
-    char tmp;
-        
-    //s = {h,e,l,l,o}
-    while (j > i){ // 4>0, 3>1, 
-        tmp = s[j]; //tmp = s[4] = o
-        s[j] = s[i]; //s[4] = s[0]   s[4] = h
-        s[i] = tmp; //s[o] = tmp = o 
-        j--;
-        i++;
-    }
 }
--- LeetCode/Easy/Top Interview Questions/2. Strings/2.Reverse Integer(0826)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/2.Reverse Integer(0826)/jaeSolution.java
@@ -1,3 +1,25 @@
 public class jaeSolution {
+    // Given a signed 32-bit integer x, return x with its digits reversed. If
+    // reversing x causes the value to go outside the signed 32-bit integer range
+    // [-231, 231 - 1], then return 0.
 
+    // Assume the environment does not allow you to store 64-bit integers (signed or
+    // unsigned).
+    // Example 1:
+
+    // Input: x = 123
+    // Output: 321
+
+    long reverse = 0;
+
+    while(x!=0){
+        int digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x /= 10;
+    }
+    if(reverse<Integer.MIN_VALUE||reverse>Integer.MAX_VALUE){
+        return 0;
+    } else {
+        return (int)reverse;
+    }
 }
