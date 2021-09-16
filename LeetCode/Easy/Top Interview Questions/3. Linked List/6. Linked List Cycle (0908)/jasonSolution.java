410e3ea105c5aa1a083f3dd8b36c377dd4365d71
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-16 14:32:09 -0700

    Update jasonSolution.java

--- LeetCode/Easy/Top Interview Questions/3. Linked List/6. Linked List Cycle (0908)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/3. Linked List/6. Linked List Cycle (0908)/jasonSolution.java
@@ -1,2 +1,25 @@
-class Solution {
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        while(head != null && fast != null && fast.next != null){
+            head = head.next;
+            fast = fast.next.next;
+            if(fast == null)
+                return false;
+            if(head == fast)
+                return true;
+        }
+        return false;
+    }
 }
