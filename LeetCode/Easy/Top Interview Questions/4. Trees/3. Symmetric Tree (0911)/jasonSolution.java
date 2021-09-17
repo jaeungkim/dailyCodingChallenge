dc822e8642cf1a43f80a8e8757dfe74451611b93
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-16 21:04:35 -0700

    Daily Coding Challenge

--- LeetCode/Easy/Top Interview Questions/4. Trees/3. Symmetric Tree (0911)/jasonSolution.java
+++ LeetCode/Easy/Top Interview Questions/4. Trees/3. Symmetric Tree (0911)/jasonSolution.java
@@ -1,2 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(preOrder1(root.left).equals(preOrder2(root.right)))
+            return true;
+        
+        return false;
+    }
+    
+    public String preOrder1(TreeNode root){
+        if(root == null)
+            return "n";
+        
+        return root.val + preOrder1(root.left) + preOrder1(root.right);
+    }
+    
+    public String preOrder2(TreeNode root){
+        if(root == null)
+            return "n";
+        
+        return root.val + preOrder2(root.right) + preOrder2(root.left);
+    }
 }
