6a240c8452ab8a814cb6fedb184d931b3bfad8c0
jaeungkim <jaeung95@student.ubc.ca>
2021-09-16 21:24:44 -0700

    catching up 0916

--- LeetCode/Easy/Top Interview Questions/2. Strings/4.Valid Anagram(0828)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/4.Valid Anagram(0828)/jaeSolution.java
@@ -1,3 +1,25 @@
 public class jaeSolution {
+    //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+    //Input s = "rat" , t="car" = false
+    //Input s = "anagram", t= "nagaram"
+    //no idea what anagram is but assuming it contains same letters
+    
+    
+    HashMap<Character,Integer> anagram = new HashMap<>();
+    
+    if(s.length() != t.length()){ // if not same length return false;
+        return false;
+    }
+    
+    for(int i = 0; i < s.length(); i++){
+        anagram.put(s.charAt(i), anagram.getOrDefault(s.charAt(i),0)+1);
+        anagram.put(t.charAt(i), anagram.getOrDefault(t.charAt(i),0)-1);
+    }
+    for(char c:anagram.keySet()){
+        if(anagram.get(c) != 0){
+            return false;
+        }
+    }
 
+    return true;
 }
--- LeetCode/Easy/Top Interview Questions/2. Strings/5.Valid Palindrome(0829)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/2. Strings/5.Valid Palindrome(0829)/jaeSolution.java
@@ -1,3 +1,25 @@
 public class jaeSolution {
-
+    /*
+    
+    Given a string s, determine if it is a palindrome, considering only alphanumeric characters             and ignoring cases.
+    
+    Input: s = "A man, a plan, a canal: Panama"
+    Output: true
+    Explanation: "amanaplanacanalpanama" is a palindrome.
+    
+    */
+    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+        while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+            i++;
+        }
+        while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+            j--;
+        }
+    
+        if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+            return false;
+        }
+    
+        return true;
+    }
 }
