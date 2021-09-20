3eee0cb2358fca54b460a41d87f122b0e0c104fa
jaeungkim <jaeung95@student.ubc.ca>
2021-09-20 13:27:00 -0700

    testing environment updated

--- LeetCode/Easy/Top Interview Questions/3. Linked List/1.Delete Node in a Linked List (0903)/jaeSolution.java
+++ LeetCode/Easy/Top Interview Questions/3. Linked List/1.Delete Node in a Linked List (0903)/jaeSolution.java
@@ -1,3 +1,18 @@
 public class jaeSolution {
-
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    public void deleteNode(ListNode node) {
+        //delete node
+        //Input: head = [-3,5,-99], node = -3
+        //Output: [5,-99]
+        
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
---
+++ jaeTesting/jaeTesting.java
@@ -0,0 +1,6 @@
+package jaeTesting;
+public class jaeTesting{
+    public static void main(String[] args){
+        System.out.println("hellO");
+    }
+}
