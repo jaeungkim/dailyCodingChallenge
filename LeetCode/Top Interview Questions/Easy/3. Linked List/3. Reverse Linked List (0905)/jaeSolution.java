301c26d7d3873027d90b02b3dd85400dbdd50c2d
jaeungkim <jaeung95@student.ubc.ca>
2021-09-22 12:33:48 -0700

    210922 leetcode

--- LeetCode/Top Interview Questions/Easy/3. Linked List/3. Reverse Linked List (0905)/jaeSolution.java
+++ LeetCode/Top Interview Questions/Easy/3. Linked List/3. Reverse Linked List (0905)/jaeSolution.java
@@ -1,3 +1,11 @@
 public class jaeSolution {
-
+    ListNode prev = null;
+    ListNode curr = head;
+    while (curr != null) {
+        ListNode nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
 }
--- LeetCode/Top Interview Questions/Easy/5. Sorting and Searching/1. Merge Sorted Array (0914)/jaeSolution.java
+++ LeetCode/Top Interview Questions/Easy/5. Sorting and Searching/1. Merge Sorted Array (0914)/jaeSolution.java
@@ -1,3 +1,7 @@
 public class jaeSolution {
-
+    //literally just merge and sort
+    for (int i = 0; i < n; i++) {
+        nums1[i + m] = nums2[i];
+    }
+    Arrays.sort(nums1);
 }
