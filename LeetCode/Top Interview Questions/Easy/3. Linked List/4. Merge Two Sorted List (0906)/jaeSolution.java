13986b493dc76f0e7ae75c0fbc14a778bf9012a9
jaeungkim <jaeung95@student.ubc.ca>
2021-09-27 15:05:29 -0700

    210927

--- LeetCode/Top Interview Questions/Easy/3. Linked List/4. Merge Two Sorted List (0906)/jaeSolution.java
+++ LeetCode/Top Interview Questions/Easy/3. Linked List/4. Merge Two Sorted List (0906)/jaeSolution.java
@@ -1,3 +1,16 @@
 public class jaeSolution {
-
+    if (l1 == null) {
+        return l2;
+    }
+    else if (l2 == null) {
+        return l1;
+    }
+    else if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+    else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
 }
--- LeetCode/Top Interview Questions/Easy/3. Linked List/5. Palindrome Linked List (0907)/jaeSolution.java
+++ LeetCode/Top Interview Questions/Easy/3. Linked List/5. Palindrome Linked List (0907)/jaeSolution.java
@@ -1,3 +1,27 @@
 public class jaeSolution {
+    public boolean isPalindrome(ListNode head) {
+        // Given the head of a singly linked list, return true if it is a palindrome.
+        List<Integer> vals = new ArrayList<>();
 
+        // Convert LinkedList into ArrayList.
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // Use two-pointer technique to check for palindrome.
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            // Note that we must use ! .equals instead of !=
+            // because we are comparing Integer, not int.
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
 }
