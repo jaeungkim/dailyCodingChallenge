eeeb3b0e22d28b69b6eb2e1a7430e54b3cced269
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-28 01:40:12 -0700

    Daily LeetCode challenge

--- LeetCode/Top Interview Questions/Easy/4. Trees/4. Binary Tree Level Order Traversal (0912)/jasonSolution.java
+++ LeetCode/Top Interview Questions/Easy/4. Trees/4. Binary Tree Level Order Traversal (0912)/jasonSolution.java
@@ -1,2 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        findOrder(root, list, 1);
+        return list;
+    }
+    
+    public void findOrder(TreeNode root, List<List<Integer>> list, int level){
+        if(root == null)
+            return ;
+        if(list.size() >= level)
+            list.get(level-1).add(root.val);
+        
+        else {
+            List<Integer> tempList = new ArrayList<Integer>();
+            tempList.add(root.val);
+            list.add(tempList);
+        }
+        findOrder(root.left, list, level + 1);
+        findOrder(root.right, list, level + 1);
+    }
 }
