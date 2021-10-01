ae386803b941247f7afeca435e5b34a0fcb5d929
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-30 19:07:01 -0700

    daily coding challenge

--- LeetCode/Top Interview Questions/Easy/5. Sorting and Searching/2. First Bad Version (0915)/jasonSolution.java
+++ LeetCode/Top Interview Questions/Easy/5. Sorting and Searching/2. First Bad Version (0915)/jasonSolution.java
@@ -1,2 +1,20 @@
-class Solution {
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n == 1)
+            return 1;
+        int f = 1;
+        int e = n;
+        int m = 0;
+        while(f < e){
+            m = f + (e-f) / 2;
+            if(isBadVersion(m))
+                e = m;
+            else 
+                f = m+1;
+        }
+        return f;
+    }
 }
