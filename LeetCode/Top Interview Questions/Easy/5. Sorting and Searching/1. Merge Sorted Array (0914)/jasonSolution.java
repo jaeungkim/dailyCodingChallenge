d876174df71168296232669a0e2d1d54e4c85746
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-09-30 18:32:17 -0700

    Daily Coding challenge

--- LeetCode/Top Interview Questions/Easy/5. Sorting and Searching/1. Merge Sorted Array (0914)/jasonSolution.java
+++ LeetCode/Top Interview Questions/Easy/5. Sorting and Searching/1. Merge Sorted Array (0914)/jasonSolution.java
@@ -1,2 +1,22 @@
 class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0)
+            return ;
+        
+        int i1 = m-1;
+        int i2 = n-1;
+        for(int i = nums1.length-1;  i >= 0; i--){
+            
+            if(i1 == -1 || nums1[i1] <= nums2[i2]){
+                nums1[i] = nums2[i2];
+                i2--;
+                if(i2 == -1)
+                    return ;
+            }
+            else {
+                nums1[i] = nums1[i1];
+                i1--;
+            }
+        }
+    }
 }
