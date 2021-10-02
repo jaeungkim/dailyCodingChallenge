8a0c69759a64f7760c1c0433718cb0743b551a15
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-10-01 21:44:17 -0700

    Daily Coding Challenge

--- LeetCode/Top Interview Questions/Easy/6. Dynamic Programming/3. Maximum Subarray (0918)/jasonSolution.java
+++ LeetCode/Top Interview Questions/Easy/6. Dynamic Programming/3. Maximum Subarray (0918)/jasonSolution.java
@@ -1,2 +1,11 @@
 class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int max = nums[0];
+        for(int i = 1 ; i < nums.length; i++){
+            maxSum = Math.max(nums[i], maxSum + nums[i]);
+            max = Math.max(max, maxSum);
+        }
+        return max;
+    }
 }
