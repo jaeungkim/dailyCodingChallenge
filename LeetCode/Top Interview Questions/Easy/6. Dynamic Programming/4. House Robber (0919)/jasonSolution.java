e17060b900101276230280f0ab331d9cf2ed5eb7
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-10-01 22:04:11 -0700

    Daily Coding Challenge

--- LeetCode/Top Interview Questions/Easy/6. Dynamic Programming/4. House Robber (0919)/jasonSolution.java
+++ LeetCode/Top Interview Questions/Easy/6. Dynamic Programming/4. House Robber (0919)/jasonSolution.java
@@ -1,2 +1,14 @@
 class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1)
+            return nums[0];
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            dp[i+1] = Math.max(dp[i], dp[i-1]+nums[i]);
+        }
+        return dp[nums.length];
+    }
 }
