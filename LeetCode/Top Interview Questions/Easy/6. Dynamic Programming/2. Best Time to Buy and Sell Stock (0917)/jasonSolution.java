2a14ee22e3247803374ea235bb9f7a9615ffe836
Jason Yoon <34023045+jayoon98@users.noreply.github.com>
2021-10-01 20:21:26 -0700

    Daily Coding Challenge

--- LeetCode/Top Interview Questions/Easy/6. Dynamic Programming/2. Best Time to Buy and Sell Stock (0917)/jasonSolution.java
+++ LeetCode/Top Interview Questions/Easy/6. Dynamic Programming/2. Best Time to Buy and Sell Stock (0917)/jasonSolution.java
@@ -1,2 +1,15 @@
 class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 1)
+            return 0;
+        int minVal = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > minVal)
+                maxProfit = Math.max(maxProfit, (prices[i]-minVal));
+            else 
+                minVal = Math.min(prices[i], minVal);
+        }
+        return maxProfit;
+    }
 }
